d1c9182fc5a6147173af065353e548edc646943b
captainbmw <drillerafro@gmail.com>
2025-09-13 01:56:41 +0300

    c learning

---
+++ hello.c
@@ -0,0 +1,32 @@
+
+
+
+/*
+Name:Brian Muiruri
+Reg:CT100/G/26172/25
+Description: To prompt the student to 
+enter height,weight,phonenumber
+*/
+
+
+#include <stdio.h>
+
+int main() {
+    int height,weight,phnNumber;
+//prompt to enter height
+	printf("Enter you height");
+	scanf("%d",&height);
+//prompt to enter weight
+	printf("Enter your weight");
+	scanf("%d",&weight);
+//prompt  to enter phone number
+	printf("Enter your phone number");
+	scanf("%d",&phnNumber);
+
+//information Display
+	printf("Height:%d metres\n",height);
+	printf("Weight:%d kg\n",weight);
+	printf("PhoneNumber: +254 %d",phnNumber);
+    return 0;
+}
+
